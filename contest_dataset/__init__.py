308ce881e00e7636eceffc338becf919db3b68e3
Ian Goodfellow <goodfellow.ian@gmail.com>
2013-02-12 14:21:37 -0500

    Made public test set load correctly

--- contest_dataset/__init__.py
+++ contest_dataset/__init__.py
@@ -53,20 +53,26 @@ class ContestDataset(DenseDesignMatrix):
         X_list = []
 
         for row in reader:
-            y_str, X_row_str = row
-            y = int(y_str)
-            y_list.append(y)
+            if which_set == 'train':
+                y_str, X_row_str = row
+                y = int(y_str)
+                y_list.append(y)
+            else:
+                X_row_str ,= row
             X_row_strs = X_row_str.split(' ')
             X_row = map(lambda x: float(x), X_row_strs)
             X_list.append(X_row)
 
         X = np.asarray(X_list)
-        y = np.asarray(y_list)
+        if which_set == 'train':
+            y = np.asarray(y_list)
 
-        one_hot = np.zeros((y.shape[0],7),dtype='float32')
-        for i in xrange(y.shape[0]):
-            one_hot[i,y[i]] = 1.
-        y = one_hot
+            one_hot = np.zeros((y.shape[0],7),dtype='float32')
+            for i in xrange(y.shape[0]):
+                one_hot[i,y[i]] = 1.
+            y = one_hot
+        else:
+            y = None
 
         view_converter = DefaultViewConverter(shape=[48,48,1])
 
